1d4155171c6acb843826aeee835bfc0f13dac011
moe399 <mahmabdul3@gmail.com>
2023-01-27 20:14:52 +0000

    first commit

---
+++ src/main/java/com/example/weatherbe/WeatherBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.weatherbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/weatherbe/WeatherBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weatherbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
